58f6db82d4c0faa420aa6ccf758d31271d468eb1
deva567 <40196767+deva567@users.noreply.github.com>
2018-07-28 20:05:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Navigation from './navbar';
+import Footer from './footer';
+import Content from './content';
+
+
+
+class App extends Component {
+  render() { 
+    return ( 
+      <div>
+      <div className="Header">
+     <Navigation />
+     </div>
+
+    <Content/>
+     
+      <div className="Footer">
+      <Footer />
+  
+      </div>
+     </div>
+     );
+  }
+}
+ 
+export default App;
